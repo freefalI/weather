181f9bc1ac5e603eec659d98c11604dbeafa45b8
freefalI <andriyshmatko1999@gmail.com>
2020-04-19 16:37:23 +0300

    area field migration

---
+++ database/migrations/2020_03_21_125521_add_area_field_to_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAreaFieldToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('latitude1');
+            $table->dropColumn('latitude2');
+            $table->dropColumn('longitude1');
+            $table->dropColumn('longitude2');
+            $table->string('area')->after('problem_type_id');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropColumn('area');
+        });
+    }
+}

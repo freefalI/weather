a8c91ccf05368a4c6025176deecf5a3dbad7fedf
freefalI <andriyshmatko1999@gmail.com>
2020-04-19 14:38:11 +0300

    add task policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Station' => 'App\Policies\StationPolicy',
+        'App\Task' => 'App\Policies\TaskPolicy',
 
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
